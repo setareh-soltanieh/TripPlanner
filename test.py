97b4b263aa88068a4f841696712f9da7f1cc35ab
setareh-soltanieh <setarehsoltanieh78@gmail.com>
2026-07-29 17:09:50 -0400

    Completed the agent in the backend.py

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 # res = tavily_search("Best Hotels in Toronto")
 # print(res)
-res = search_flights("Plan a 7 days Iran trip from Toronto")
-print(res)
+# res = search_flights("Plan a 7 days Iran trip from Toronto")
+# print(res)
+
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input, 
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])

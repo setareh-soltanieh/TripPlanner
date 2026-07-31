3e0464b9cfb6ee5c2bdfb3ac30394c71dcce467e
setareh-soltanieh <setarehsoltanieh78@gmail.com>
2026-07-31 09:42:54 -0400

    Added the tavily mcp client to the code

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
-from tools.tavily_tool import tavily_search
-from tools.flight_tool import search_flights
-from backend import run_travel_agent
-# res = tavily_search("Best Hotels in Toronto")
-# print(res)
-# res = search_flights("Plan a 7 days Iran trip from Toronto")
-# print(res)
+import asyncio
+from mcp_client_test import get_all_tools, tavily_mcp_search
 
-user_input = input("Enter travel request: ")
-
-response = run_travel_agent(
-    user_input=user_input, 
-    thread_id="test_user"
-)
-
-print("\nFINAL RESPONSE:\n")
-print(response["answer"])
+if __name__ == "__main__":
+    query = "latest news about AI"
+    asyncio.run(tavily_mcp_search(query))
